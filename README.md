6e949c220336bdb4f55fef4a9ab6e2893d16dad0
sungkwangLim <limsungkwang@naver.com>
2021-06-08 00:19:28 +0900

    데탑 첫 연결 및 기본구조 만들기 및 첫 커밋

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LetsStudy_JS_Chapter1
 Let's get start Javascript study
+
+jquery쓰지말고 순수 JS로만 천천히 공부 시작해 봅시다. 
---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+document.write('Hello World SK!')
+
+const divCont = document.createElement('div');
+divCont.textContent = 'Hello JH'
+document.body.append(divCont);
