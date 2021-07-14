95fd6294a83780654bddf05f7609790574010d61
sungkwangLim <limsungkwang@naver.com>
2021-07-15 00:00:16 +0900

    유튜브 코딩애플보고 따라하기 4강 완료

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,10 +33,20 @@ tabEvt.click(tabMenu,tabCont);
 //html을 먼저 불러오고 js를 실행해야 버그가 안생김
 //js 선택자를 잘 보고 컨트롤해야함
 //Chrome 개발자 모드 콘솔창에서 버그 찾고 수정하는 방법 익숙해지기
-function 알림창열기(){
-    document.querySelector('.alert-box').style.display = 'block'
+function 알림창열기(구멍,구멍2){
+    document.querySelector('.alert-box').style.display = 구멍;
+    document.querySelector('.alert-box > p').innerHTML = 구멍2;
 }
 
-function 알림창닫기(){
-    document.querySelector('.alert-box').style.display = 'none'
-}
+// 파라메터값 넣는거 배우면서 아래 코드는 중복코드라 주석처리
+// function 알림창닫기(){
+//     document.querySelector('.alert-box').style.display = 'none'
+// }
+
+/* 
+
+ 20210714 코딩애플4강 숙제 -> html은 냅두고 버튼1, 버튼2 눌렀을때 파라메터값을 이용해서
+ '아이디를 입력안하셨어요'
+ '비번을 입력안하셨어요'
+ 라고 텍스트 표시하게 만들기
+*/
